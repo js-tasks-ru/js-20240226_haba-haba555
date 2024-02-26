dd7d7d928c6da0fede7677b67ed98bf1acf56c2b
Liu Wen <haba-haba555@ya.ru>
2024-02-27 00:07:13 +0300

    3rd task: create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
 
+  return function(obj) {
+    let newObj = obj;
+    for (let i = 0; i < props.length - 1; i++) {
+      const currentProp = props[i];
+      if (newObj[currentProp]) {
+        newObj = newObj[currentProp];
+      }
+    }
+    return newObj[props[props.length - 1]];
+  };
 }
