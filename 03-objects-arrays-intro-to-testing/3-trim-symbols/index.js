dbd5d0b11e78787e27773d1124f07bdbaebb3ac4
Liu Wen <haba-haba555@ya.ru>
2024-02-28 23:23:23 +0300

    3rd task: trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string) return '';
 
+  const [firstLetter, ...others] = [...string];
+  const repeatedStrings = [];
+  let currentString = firstLetter;
+
+  for (let i = 0; i < others.length; i++) {
+    if (currentString.at(-1) === others[i]) {
+      currentString += others[i];
+    } else {
+      repeatedStrings.push(currentString);
+      currentString = others[i];
+    }
+  }
+  repeatedStrings.push(currentString);
+
+  const trimmedStrings = [];
+  for (const str of repeatedStrings) {
+    trimmedStrings.push(str.slice(0, size));
+  }
+
+  return trimmedStrings.join('');
 }
