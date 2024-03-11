d04bfa6f1263535fd325fd1a161b1ea22c82353d
Liu Wen <haba-haba555@ya.ru>
2024-03-12 00:05:51 +0300

    5th task: notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,70 @@
 export default class NotificationMessage {
+  static lastInstance;
+  element;
+  timerId;
 
+  constructor(message = '', options = {}) {
+    this.message = message;
+    this.duration = options.duration || 2000;
+    this.type = options.type || 'success';
+    this.element = this.createNotification(this.createNotificationTemplate());
+  }
+
+  createNotification(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createNotificationTemplate() {
+    return (`
+      <div class="notification ${this.type}" style="--value:20s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  setNotificationTimer() {
+    this.timerId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  unsetNotificationTimer() {
+    clearTimeout(this.timerId);
+  }
+
+  setNotificationLastInstance() {
+    if (NotificationMessage.lastInstance) {
+      NotificationMessage.lastInstance.destroy();
+    }
+    NotificationMessage.lastInstance = this;
+  }
+
+  show(wrapper) {
+    this.setNotificationLastInstance();
+
+    if (wrapper) {
+      wrapper.appendChild(this.element);
+    } else {
+      document.body.appendChild(this.element);
+    }
+
+    this.setNotificationTimer();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.unsetNotificationTimer();
+    this.remove();
+  }
 }
